d5bfa23212681f1ba661fcc2a8e8e798d3cb4e0f
Michal Ostrowski <michal.ostrowski@forlagshuset.no>
2022-04-27 14:17:50 +0200

    feat: add packages files

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { AuditLogger } from './src/audit-logging.decorator';
+export { AuditLoggingInterceptor } from './src/audit-logging.interceptor';
+export * from './src/audit-logging.interfaces';
