16c206861dd5ea0cc3589024a75c58eaffc822d4
Michal Ostrowski <michal.ostrowski@forlagshuset.no>
2022-04-28 14:21:49 +0200

    feat: re-export AuditLog

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { AuditLogger } from './audit-logging.decorator';
 export { AuditLoggingInterceptor } from './audit-logging.interceptor';
 export * from './audit-logging.interfaces';
-export * from '@forlagshuset/audit-logging';
+export { AuditLog } from '@forlagshuset/audit-logging';
