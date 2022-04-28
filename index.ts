e0b7d2cd92d803cabf0f82436b8e228758dd945c
Michal Ostrowski <michal.ostrowski@forlagshuset.no>
2022-04-28 14:43:14 +0200

    fix: remove unnecessary file

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 export { AuditLogger } from './src/audit-logging.decorator';
 export { AuditLoggingInterceptor } from './src/audit-logging.interceptor';
 export * from './src/audit-logging.interfaces';
+
+export * from '@forlagshuset/audit-logging';
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { AuditLogger } from './audit-logging.decorator';
-export { AuditLoggingInterceptor } from './audit-logging.interceptor';
-export * from './audit-logging.interfaces';
-
-import { AuditLog } from '@forlagshuset/audit-logging';
-export { AuditLog };
